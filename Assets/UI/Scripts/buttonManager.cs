89707b604c211096fa4e2cc560b11c108e143cb6
Ethantl28 <ethantl28@outlook.com>
2024-01-16 13:50:30 +0000

    added simple main menu

---
+++ Assets/UI/Scripts/buttonManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        //Start the game
+    }
+
+    public void OpenSettings()
+    {
+        //open settings menu
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
