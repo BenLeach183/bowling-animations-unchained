890d1ff55c11f5891b44c2caf3392e7eb14787ef
BenLeach <p2764602@my365.dmu.ac.uk>
2024-01-16 19:29:32 +0000

    Added system to connect tracks together

---
+++ Assets/Scripts/FloorTrackObject.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+  Each track object has this script
+  it stores the position and rotation
+  of the track, and will connect to other
+  tracks.
+ */
+
+public class FloorTrackObject : MonoBehaviour
+{
+    // whether the track has been connected to the next track
+    public bool connected = false;
+
+    // whether the track is enabled
+    public bool trackEnabled = false;
+
+    // store the local end and start pos/rot of track
+    private Vector3 localStartPos;
+    private Vector3 localEndPos;
+
+    private Quaternion localStartRot;
+    private Quaternion localEndRot;
+
+    // store the actual end pos/rot of track
+    public Vector3 endPos;
+    public Quaternion endRot;
+
+    // update the default values
+    public void Awake()
+    {
+        Debug.Log("Awake");
+        this.gameObject.SetActive(false);
+
+        localStartPos = transform.Find("procedural_start").position;
+        localStartRot = transform.Find("procedural_start").rotation;
+
+        localEndPos = transform.Find("procedural_end").position;
+        localEndRot = transform.Find("procedural_end").rotation;
+
+        endPos = localEndPos;
+        endRot = localEndRot;
+
+    }
+
+    public bool ConnectToPoint(Vector3 position, Quaternion rotation)
+    {
+        // find the rotation ( multiplying by inverse subtracts rotation)
+        Quaternion rotateTo = rotation * Quaternion.Inverse(localStartRot);
+        
+        // update the rotation
+        this.transform.rotation = rotateTo;
+
+        // calculate position to move to
+        // the local start pos is the offset from objects origin
+        // rotate the local pos
+        Vector3 newPos = position - (rotateTo*localStartPos);
+        this.transform.position = newPos;
+
+        // update the new end position and rotation
+        endPos = newPos + (rotateTo*localEndPos);
+        endRot = rotateTo*localEndRot;
+
+        // activate the object
+        this.gameObject.SetActive(true);
+        trackEnabled = true;
+
+        // return true if succesful
+        return true;
+    }
+
+    public void EnableTrack()
+    {
+        trackEnabled = true;
+        this.gameObject.SetActive(true);
+    }
+
+    public void Despawn()
+    {
+        connected = false;
+        trackEnabled = false;
+        this.gameObject.SetActive(false);
+    }
+}
