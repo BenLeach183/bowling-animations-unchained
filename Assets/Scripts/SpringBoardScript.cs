5f7dc9392990a415b4509cb4383929591e7335a4
Kamil Turecki <P2766269@LEC-ADMIN.dmu.ac.uk>
2024-01-22 12:17:43 +0000

    Updated smoothing on models and gave functionality to spring asset

---
+++ Assets/Scripts/SpringBoardScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringBoardScript : MonoBehaviour
+{
+    Animator animator;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Player")
+        {
+            other.transform.GetComponent<Rigidbody>().AddForce(other.transform.GetComponent<PlayerController>().CurrentUpDirection * 2500, ForceMode.Force);
+            animator.SetBool("Triggered", true);
+            Destroy(GetComponent<BoxCollider>());
+        }
+    }
+}
