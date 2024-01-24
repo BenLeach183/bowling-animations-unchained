23681ba3ef13eac36b0835a85a11abe4a2f0cafc
Ethantl28 <ethantl28@outlook.com>
2024-01-24 23:37:45 +0000

    Made bat, plank, and electric pole 1 model, added splines to connect poles together and made a connected pole prefab

---
+++ Assets/BreakObjectParticle.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BreakObjectParticle : MonoBehaviour
+{
+    private ParticleSystem particles;
+    private BoxCollider thisCollision;
+    MeshRenderer meshRenderer;
+    Color transparent;
+    private bool destroyed;
+
+    public float lifespan = 0.5f;
+    public bool pushPlayerBack = true;
+    public float pushBackForce = -50f;
+
+    private float t = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particles = GetComponentInChildren<ParticleSystem>();
+        thisCollision = GetComponent<BoxCollider>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        transparent = new Color(0, 0, 0, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            particles.Play();
+            meshRenderer.material.color = transparent;
+            thisCollision.enabled = false;
+            Destroy(this.gameObject, lifespan);
+
+            if (pushPlayerBack)
+            {
+                collision.gameObject.GetComponent<PlayerController>().ExtraForce += collision.gameObject.GetComponent<Rigidbody>().velocity.normalized * collision.gameObject.GetComponent<PlayerController>().speed * pushBackForce;
+                Debug.Log("LMAO u lost points xD");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ElectricCableSpline.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ElectricCableSpline : MonoBehaviour
+{
+    public Transform startPoint, endPoint;
+    private Vector3 a, b, c;
+    private LineRenderer lineRenderer;
+    int segments = 6;
+    private List<Vector3> points = new List<Vector3>();
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        a = startPoint.position;
+        c = endPoint.position;
+
+        Vector3 droop = (a + ((c - a) / 2)) - (Vector3.up * (Vector3.Distance(a, c) / 10));
+        b = droop;
+
+        RasterizeBezier();
+
+        lineRenderer.positionCount = segments + 1;
+        lineRenderer.SetPositions(points.ToArray());
+    }
+
+    private void RasterizeBezier()
+    {
+        for (int i = 0; i <= segments; i++)
+        {
+            points.Add(GetValue((float)i / segments));
+        }
+    }
+
+    Vector3 GetValue(float t)
+    {
+        Vector3 ab, bc;
+
+        ab = GetT(a, b, t);
+        bc = GetT(b, c, t);
+        return GetT(ab, bc, t);
+    }
+
+    Vector3 GetT(Vector3 a, Vector3 b, float t)
+    {
+        return a + ((b - a) * t);
+    }
+}
