c62a987ad97279269446c50d40a13914179e9237
BenLeach <p2764602@my365.dmu.ac.uk>
2024-01-27 15:38:53 +0000

    increase fov in portrait mode

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject Player;
+
     void Update()
     {
         transform.position = Player.transform.position;
         transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.LookRotation(Player.GetComponent<PlayerController>().CurrentMoveDirection, Player.GetComponent<PlayerController>().CurrentUpDirection),Time.deltaTime * Player.GetComponent<PlayerController>().speed);
+
+        // get the aspcet ratio of the screen
+        float aspect = Screen.width / Screen.height;
+
+        // if the screen width is less than height increase fov
+        if(aspect < 1)
+        {
+            
+            Camera.main.fieldOfView = 100;
+        } else
+        {
+            Camera.main.fieldOfView = 60;
+        }
     }
 }
