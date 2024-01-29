f4d2a0be31b907aa318ced22767143b45f70c16b
BenLeach <p2764602@my365.dmu.ac.uk>
2024-01-29 17:08:57 +0000

    Updated consistentFOV for portrait

--- Assets/UI/Scripts/consistentFOV.cs
+++ Assets/UI/Scripts/consistentFOV.cs
@@ -4,28 +4,45 @@ using UnityEngine;
 
 public class consistentFOV : MonoBehaviour
 {
-    public float baseFOV = 31f;
+    private float aspect;
+    public float landscapeBaseFOV = 31f;
+    public float portraitBaseFOV = 27.2f;
 
     Camera mainCamera;
 
     private void Start()
     {
         mainCamera = GetComponent<Camera>();
-        AdjustFOV();
+        //AdjustFOV();
     }
 
-    private void AdjustFOV()
+    private void AdjustFOV(float baseFOV, float targetAspect)
     {
-        float targetAspect = 16f / 9f;
-        float currentAspect = (float)Screen.width / Screen.height;
-
-        float adjustedFOV = baseFOV * (targetAspect / currentAspect);
+        float adjustedFOV = baseFOV * (targetAspect / aspect);
 
         mainCamera.fieldOfView = adjustedFOV;
     }
 
     private void Update()
     {
-        AdjustFOV();    //This is on update for debug purposes REMOVE THIS after testing
+        // get the current aspect of the screen
+        aspect = (float)Screen.width / Screen.height;
+
+        // if it is wider than tall use landscape fov
+        if(aspect > 1)
+        {
+            // adjusts the fov to keep scale correct
+            AdjustFOV(landscapeBaseFOV, 16f/9f);
+        } 
+        // if it is within aspect 0.7 - 1.0 use a set fov
+        else if (aspect > 0.7f)
+        {
+            mainCamera.fieldOfView = 27.2f;
+        } 
+        // if it is portait and less then 0.7 update fov
+        else
+        {
+            AdjustFOV(portraitBaseFOV, 9f / 16f);
+        }
     }
 }
